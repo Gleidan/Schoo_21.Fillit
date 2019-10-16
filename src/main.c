f0a5b79264043e6e3ee44baa9ba0b94539f2436a
cyberforge12 <nicolasC12>
2019-10-16 14:53:03 +0300

    added execution time print to main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include "fillit.h"
+#include <sys/time.h>
+#include <stdio.h>
 
 void	ft_free_list(void *content, size_t size)
 {
@@ -20,6 +22,8 @@ int		main(int argc, char **argv)
 	t_list	*list;
 	char	**map;
 	int		size;
+	struct timeval  tv1, tv2;
+	gettimeofday(&tv1, NULL);
 
 	if (argc != 2)
 	{
@@ -35,5 +39,12 @@ int		main(int argc, char **argv)
 	ft_clear_table(map, size);
 	ft_lstdel(&list, &ft_free_list);
 	close (fd);
+	gettimeofday(&tv2, NULL);
+	printf ("Total time = %f seconds\n",
+			(double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+			(double) (tv2.tv_sec - tv1.tv_sec));
 	return (0);
+
+
+
 }
